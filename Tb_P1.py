9f13f34e7e0b3e4a07ef0e6143e2dfea3ac8405a
Thiago Santos <thiago.santos@smartnx.com>
2024-05-04 16:02:36 -0300

    Aplicação de boas práticas em python.

--- Tb_P1.py
+++ Tb_P1.py
@@ -1,11 +1,16 @@
 import numpy as np
 from scipy.stats import chi2_contingency
 
+def quiQuadrado(data):
+    try:
+        chi2, p, dof, expected = chi2_contingency(data)
+        print(f"qui-quadrado: {chi2}")
+        print(f"p-value: {p}")
+        print(f"Grau de liberdade: {dof}")
+        print(f"Valor esperado:\n {expected}")
+    except:
+        print("Ocorreu um erro ao executar o teste qui-quadrado")
+
 data = np.loadtxt('dados.csv', delimiter=',', skiprows=1, usecols=range(1, 5))
 
-chi2, p, dof, expected = chi2_contingency(data)
-print(f"qui-quadrado: {chi2}")
-print(f"p-value: {p}")
-print(f"Grau de liberdade: {dof}")
-print(f"Valor esperado:\n {expected}")
-
+quiQuadrado(data)
